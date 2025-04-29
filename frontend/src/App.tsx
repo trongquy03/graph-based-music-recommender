8683340d3043efe68ea0b2180da518dd221dac54
Trong Quy <trongquy1628@gmail.com>
2025-04-29 18:29:12 +0700

    initialize frontend project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  
+
+  return (
+   <>
+   <h1 className="text-red-500 text-5xl">music</h1>
+   <Button variant={"outline"}> This is a button</Button>
+   </>
+  )
+}
+
+export default App
