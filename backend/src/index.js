58891ee14775292b6d99c2973dc282ce64c096a6
Trong Quy <trongquy1628@gmail.com>
2025-05-04 23:38:58 +0700

    Add createSong logic with Cloudinary upload and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"
 import { clerkMiddleware } from '@clerk/express'
+import fileUpload from "express-fileupload"
+import path from "path"
 
 import userRoutes from "./routes/user.route.js"
 import adminRoutes from "./routes/admin.route.js"
@@ -8,17 +10,25 @@ import authRoutes from "./routes/auth.route.js"
 import albumRoutes from "./routes/album.route.js"
 import songRoutes from "./routes/song.route.js"
 import statRoutes from "./routes/stat.route.js"
-import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // parse req.body
-
 app.use(clerkMiddleware()); // auth request
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+        fieldSize: 10 * 1024 * 1024 // max 10mb
+        
+    }
+}))
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -26,6 +36,11 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+// error handle
+app.use((err,req,res,next) => {
+    res.status(500).json({message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message});
+})
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
     connectDB();
