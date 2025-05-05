e74d4053754e3514765b469376cd1e0da0273efc
Trong Quy <trongquy1628@gmail.com>
2025-05-05 21:41:28 +0700

    implement AuthProvider to manage token with Clerk and axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,16 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
-import { Button } from "./components/ui/button"
+
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { axiosInstance } from "./lib/axios";
 
 function App() {
   return (
     <>
-    <header>
-    <SignedOut>
-      <SignInButton>
-        <Button>
-          Sign In
-        </Button>
-      </SignInButton>
-    </SignedOut>
-    <SignedIn>
-      <UserButton />
-    </SignedIn>
-  </header>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
+    </Routes>
     </>
   );
 }
