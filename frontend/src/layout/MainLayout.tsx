dc470ab00627c4644b8b989976a07f1909c302db
Trong Quy <trongquy1628@gmail.com>
2025-05-09 03:48:42 +0700

    Create Playback Controls Component

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -3,9 +3,21 @@ import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "@/layout/components/AudioPlayer";
+import PlaybackControls from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-    const isMobile = false;
+    const [isMobile, setIsMobile] = useState(false);
+    
+    useEffect(() => {
+		const checkMobile = () => {
+			setIsMobile(window.innerWidth < 768);
+		};
+
+		checkMobile();
+		window.addEventListener("resize", checkMobile);
+		return () => window.removeEventListener("resize", checkMobile);
+	}, []);
     return (
         <div className="h-screen bg-black text-white flex flex-col">
             <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
@@ -22,14 +34,19 @@ const MainLayout = () => {
                     <Outlet />
                 </ResizablePanel>
 
-                <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-
-                {/* Right sidebar */}
-                <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
-                    <FriendsActivity/>
-                </ResizablePanel>
+                {!isMobile && (
+                    <>
+                    <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+                        {/* Right sidebar */}
+                        <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
+                            <FriendsActivity/>
+                        </ResizablePanel>
+                    </>
+                )}
 
             </ResizablePanelGroup>
+
+            <PlaybackControls />
         </div>
   )
 }
