c7c5fd86e4001aeadeb1e5666b5ef4a5daae7701
Trong Quy <trongquy1628@gmail.com>
2025-05-15 05:42:46 +0700

    Add NotFoundPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import AlbumPage from "./pages/album/AlbumPage";
 import AdminPage from "./pages/admin/AdminPage";
 
 import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<HomePage/>} />
           <Route path="/chat" element={<ChatPage/>} />
           <Route path="/albums/:albumId" element={<AlbumPage/>} />
+          <Route path="/*" element={<NotFoundPage/>} />
       </Route>
     </Routes>
     <Toaster/>
