b48f06fedd81886357e2ea6585422816625493dc
Trong Quy <trongquy1628@gmail.com>
2025-07-01 20:49:05 +0700

    update

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -6,7 +6,7 @@ import { getAllUsers, getPremiumStatus } from "../controller/user.controller.js"
 const router = Router();
 
 router.get("/",protectRoute, getAllUsers);
-router.get("/messages/:userId",protectRoute, getAllUsers);
+// router.get("/messages/:userId",protectRoute, getAllUsers);
 router.get("/me/premium-status", protectRoute, getPremiumStatus);
 
 export default router
