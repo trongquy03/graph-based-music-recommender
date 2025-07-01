546d6a5c4617142a34853b28555853e8727b27d6
Trong Quy <trongquy1628@gmail.com>
2025-07-01 19:54:32 +0700

    deploy

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
 });
 axiosInstance.interceptors.request.use(async (config) => {
   const token = await window.Clerk?.session?.getToken();
--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -22,8 +22,8 @@ interface ChatStore {
 	setSelectedUser: (user: User | null) => void;
 }
 
-// const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
-const baseURL = "http://localhost:5000";
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+// const baseURL = "http://localhost:5000";
 
 const socket = io(baseURL, {
 	autoConnect: false, // only connect if user is authenticated
