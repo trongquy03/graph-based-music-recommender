c251148975d3811afb933f0ee4c9e80c6f36f9f5
Trong Quy <trongquy1628@gmail.com>
2025-07-01 19:53:53 +0700

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,9 @@ import { clerkMiddleware } from '@clerk/express'
 import fileUpload from "express-fileupload"
 import path from "path"
 import cors from "cors"
+import fs from "fs";
 import { createServer } from "http";
+import cron from "node-cron";
 
 import { initializeSocket } from "./lib/socket.js";
 
@@ -57,6 +59,23 @@ app.use(fileUpload({
     }
 }))
 
+// cron jobs
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
+
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
@@ -75,7 +94,12 @@ app.use("/api/ai-chat", aiChatRoutes);
 app.use("/api/generate", generateRoutes);
 app.use("/api/cloudinary", cloudinaryRoutes);
 
-
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+	});
+}
 
 // error handle
 app.use((err, req, res, next) => {
